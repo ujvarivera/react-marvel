598d0f5b113ed00c8ef6590b5c2f23bbb214659f
ujvarivera <ujvariveronika0320@gmail.com>
2024-04-18 15:51:32 +0200

    fetching characters from marvel api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
+  const URL = import.meta.env.VITE_MARVEL_API
+
+  useEffect(() => {
+    const getCharacters = async (url) => {
+      const { data } = await axios.get(url);
+      setCharacters(data.data.results)
+      console.log(data.data.results)
+      setLoading(false)
+    }
+    getCharacters(URL)
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+        loading ?
+          <p>Loading...</p> :
+          <div>
+            {
+              characters?.map((character) => (
+                <div>
+                  <img key={character.id} src={`${character?.thumbnail.path}.${character?.thumbnail.extension}`} alt={character?.name} />
+                  <p>{character?.name}</p>
+                </div>
+              ))
+            }
+          </div>
+      }
+    </div>
   )
 }
 
